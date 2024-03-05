68429a324bfe3804d6ba2f46f4a3c463723faef6
aartijangra <aartijangra482@gmail.com>
2024-03-06 02:15:19 +0530

    DOM

--- arrays.js
+++ arrays.js
@@ -3,4 +3,17 @@ var firstval = ourArray[0];
 console.log(firstval);
 
 //nested array
-var array = [["universe", 10],["moon", 20],["sun", 30]];
+var myarray = [["universe", 10],["moon", 20],["sun", 30]];
+
+//array is an special type of object
+// it has properties and methods
+console.log(typeof [1,2]);
+console.log(Array.isArray([1,2]));
+console.log(myarray.length);
+
+myarray.push(100);
+console.log(myarray);
+
+myarray.splice(0,2); //to delete value(index,no.of valuues)
+
+
